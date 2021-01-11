6664fa25fb5fdd5be40f2a1416d648cd3f8abb10
Konrad Pardyak <konradpardyak@gmail.com>
2021-01-11 20:20:55 +0100

    Add reset progress function to Settings

--- src/Settings/Settings.js
+++ src/Settings/Settings.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import { useDispatch } from "react-redux";
+import { setProgress } from "../redux/ducks/progress";
 
 const Settings = () => {
+  const dispatch = useDispatch();
+
+  const handleSetProgress = () => {
+    dispatch(setProgress(0));
+  }
+
   return (
     <div>
       <h1>Settings</h1>
+      <button onClick={handleSetProgress}>Reset progress</button>
     </div>
   );
 }
